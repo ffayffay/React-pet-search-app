f1a9711d8d844c8fe2fd6cdefcef9bb7c1f9ad27
Fay Marie <samsongreen72@gmail.com>
2018-11-14 20:11:23 -0500

    Updates

---
+++ precache-manifest.50531509a65a4f4147697d93bc715e73.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a17d1358d09d851e3a80",
+    "url": "/React-pet-search-app/static/js/runtime~main.a17d1358.js"
+  },
+  {
+    "revision": "c8c30f26571043810c66",
+    "url": "/React-pet-search-app/static/js/main.c8c30f26.chunk.js"
+  },
+  {
+    "revision": "a5bdc2e54c0cc5232671",
+    "url": "/React-pet-search-app/static/js/1.a5bdc2e5.chunk.js"
+  },
+  {
+    "revision": "c8c30f26571043810c66",
+    "url": "/React-pet-search-app/static/css/main.fed4c381.chunk.css"
+  },
+  {
+    "revision": "bf2093806b88d6c301b5d404f59eec11",
+    "url": "/React-pet-search-app/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.9e98521aac46d0e84a7466d93e4973be.js"
+  "/React-pet-search-app/precache-manifest.50531509a65a4f4147697d93bc715e73.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/React-pet-search-app/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
