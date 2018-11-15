ac733727196cb7680a75fb0853942705b0bf4b13
Fay Marie <samsongreen72@gmail.com>
2018-11-14 20:06:00 -0500

    Updates

---
+++ precache-manifest.9e98521aac46d0e84a7466d93e4973be.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "2515c5785da40b5dfe2c",
+    "url": "/static/js/main.2515c578.chunk.js"
+  },
+  {
+    "revision": "341220da4d03d6862c15",
+    "url": "/static/js/1.341220da.chunk.js"
+  },
+  {
+    "revision": "2515c5785da40b5dfe2c",
+    "url": "/static/css/main.fed4c381.chunk.css"
+  },
+  {
+    "revision": "9e36611488fb47235ae6eb0184bb325c",
+    "url": "/index.html"
+  }
+];
